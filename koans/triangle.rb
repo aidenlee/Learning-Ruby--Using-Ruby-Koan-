861a1b9aa6f490df66ab033b1f449fdd96d1e0ba
Aiden Lee <aiden.jihun.lee@gmail.com>
2013-01-04 14:12:10 -0500

    Create triangle fn and added exceptions

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+    
+  	raise TriangleError, "Sides must be numbers greater than zero" if (a <= 0) || (b <= 0) || (c <= 0)
+  	raise TriangleError, "No two sides can add to be less than or equal to the other side" if ((a + b) <= c) || ((a + c) <= b) || ((c + b) <= a) 
+   
+  	if a == b && b == c
+ 		return :equilaterial
+  	end
+  	if (a == b || b == c || a == c) && !(a == b && b ==c) 
+		return :isosceles
+	end
+	if (a != b && a != c && b != c)
+		return :scalene
+	end
+  	
 end
 
 # Error class used in part 2.  No need to change this code.
